b1ea7d4206bf3994446914561987017c47382f0e
Demayne Govender <govender.demayne@gmail.com>
2025-04-25 00:08:34 +0200

    Add .gitignore to ignore class files and build directories

--- bin/doc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"<Unnamed>","c":"DatabaseConnection","l":"DatabaseConnection()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"DatabaseConnection","l":"getConnection()"}];updateSearchResults();
